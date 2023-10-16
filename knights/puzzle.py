fe56bb8d235a4305d766ad840c98a261300876a8
alextywoo <132611126+alextywoo@users.noreply.github.com>
2023-10-16 09:58:32 -0700

    Add files via upload

---
+++ knights/puzzle.py
@@ -0,0 +1,76 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(Or(And(AKnight,Not(AKnave)), And(AKnave,Not(AKnight))),
+                 Or(And(AKnight, And(AKnight, AKnave)), And(AKnave, Not(And(AKnight, AKnave))))
+    # TODO
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And( Or(And(AKnight,Not(AKnave)), And(AKnave,Not(AKnight))),
+                  Or(And(BKnight,Not(BKnave)), And(BKnave,Not(BKnight))),
+                  Or(And(AKnight, And(AKnave, BKnave)), And(AKnave, Not(And(AKnave, BKnave))))
+    # TODO
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(Or(And(AKnight, Or(And(AKnave, BKnave),And(AKnight, BKnight))),
+                    And(AKnave, Not(And(AKnave, BKnave)), Not(And(AKnight, BKnight)))),
+                    Or(And(BKnight, Or(And(AKnave, BKnight),And(AKnight, BKnave))),
+                    And(BKnave, Not(And(AKnave, BKnight)), Not(And(AKnight, BKnave)))),
+                    Or(And(AKnight,Not(AKnave)), And(AKnave,Not(AKnight))),
+                    Or(And(BKnight,Not(BKnave)), And(BKnave,Not(BKnight)))
+    # TODO
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(Or(And(AKnight,Not(AKnave)), And(AKnave,Not(AKnight))),
+                 Or(And(BKnight,Not(BKnave)), And(BKnave,Not(BKnight))),
+                 Or(And(CKnight,Not(CKnave)), And(CKnave,Not(CKnight))),
+                 Or(And(BKnight, CKnave), And(BKnave, CKnight)),
+                 Or(And(CKnight, AKnight), And(CKnave, AKnave)),
+                 Or(And(BKnight, Or(And(AKnight, BKnave), And(AKnave, Not(BKnave)))),
+                    And(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, Not(BKnave)))))),
+
+    # TODO
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
