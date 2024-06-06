1c3de4bdb61f7fdb66054f64bdb65352a04a92d5
Alex Wu <132611126+alextywoo@users.noreply.github.com>
2024-06-05 20:57:09 -0700

    Add files via upload

---
+++ parser/parser.py
@@ -0,0 +1,100 @@
+import nltk
+import sys
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP
+NP -> Det N | Det AdjP N | N | AdjP N | NP PP | N Conj NP | NP Conj NP
+VP -> V | V NP | V Adv | V NP PP | V PP | VP Conj VP
+AdjP -> Adj | Adj AdjP
+PP -> P NP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    words = word_tokenize(sentence.lower())
+    words = [word for word in words if word.isalpha()]
+    return words
+
+
+def np_chunk(tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    noun_phrases = []
+
+    # Traverse the tree to find all subtrees labeled "NP"
+    for subtree in tree.subtrees():
+        if subtree.label() == 'NP':
+            # Check if the current NP subtree contains any other NP subtrees
+            contains_np_subtree = any(
+                child_subtree.label() == 'NP' for child_subtree in subtree if isinstance(child_subtree, nltk.Tree))
+            if not contains_np_subtree:
+                noun_phrases.append(subtree)
+
+    return noun_phrases
+
+
+
+
+if __name__ == "__main__":
+    main()
